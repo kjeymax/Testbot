26e84d24ef00f1975d2bd247985940a464295a0e
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2024-06-08 23:49:45 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv(" "))
+API_HASH = int(getenv(" "))
+BOT_TOKEN = int(getenv(" "))
+
+MONGO_DB_URI = int(getenv(" "))
+
+INDEX_ID = int(getenv(" "))
+UPLOADS_ID = int(getenv(" "))
+
+STATUS_ID = int(getenv(" "))
+SCHEDULE_ID = int(getenv(" "))
+
+CHANNEL_TITLE = int(getenv(" "))
+INDEX_USERNAME = int(getenv(" "))
+UPLOADS_USERNAME = int(getenv(" "))
