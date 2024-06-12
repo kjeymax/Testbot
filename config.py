53a4aab6cebd56c2a6d0ad8b2f2da5333a722255
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2024-06-13 00:12:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,37 @@
-from os import getenv
+import os
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists("config.env"):
+    load_dotenv('config.env')
+elif os.path.exists("sample.env"):
+    load_dotenv("sample.env")
 
-API_ID = int(getenv(" "))
-API_HASH = int(getenv(" "))
-BOT_TOKEN = int(getenv(" "))
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+UPLOAD_ID = os.getenv("UPLOAD_ID")
+INDEX_ID  = os.getenv("INDEX_ID ")
+STATUS_ID = os.getenv("STATUS_ID")
+SCHEDULE_ID = os.getenv("SCHEDULE_ID")
+CHANNEL_TITLE = os.getenv("CHANNEL_TITLE", "H-Anime")
+INDEX_USERNAME = os.getenv("INDEX_USERNAME")
+UPLOADS_USERNAME = os.getenv("UPLOADS_USERNAME")
+COMMENTS_GROUP_LINK = os.getenv("COMMENTS_GROUP_LINK")
+SLEEP_TIME = os.getenv("SLEEP_TIME", 15)
 
-MONGO_DB_URI = int(getenv(" "))
-
-INDEX_ID = int(getenv(" "))
-UPLOADS_ID = int(getenv(" "))
-
-STATUS_ID = int(getenv(" "))
-SCHEDULE_ID = int(getenv(" "))
-
-CHANNEL_TITLE = int(getenv(" "))
-INDEX_USERNAME = int(getenv(" "))
-UPLOADS_USERNAME = int(getenv(" "))
+for k, v in {
+    "API_ID": API_ID,
+    "API_HASH": API_HASH,
+    "BOT_TOKEN": BOT_TOKEN,
+    "MONGO_DB_URI": MONGO_DB_URI,
+    "STATUS_ID": STATUS_MSG_ID,
+    "SCHEDULE_ID": SCHEDULE_MSG_ID,
+    "UPLOAD_ID": UPLOAD_ID
+    "INDEX_ID": INDEX_ID   
+    "INDEX_USERNAME": INDEX_CHANNEL_USERNAME,
+    "UPLOADS_USERNAME": UPLOADS_CHANNEL_USERNAME,
+    "COMMENTS_GROUP_LINK": COMMENTS_GROUP_LINK,
+}.items():
+    if not v:
+        raise Exception(f"{k} not found .env file, please add it to use H-Anime Bot")
